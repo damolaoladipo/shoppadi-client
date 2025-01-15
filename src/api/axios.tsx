18f96c1c32c04ba82e2c1568507bb1bbf76d5b3d
Damola Oladipo <damolaoladipo@users.noreply.github.com>
2025-01-15 19:53:00 +0100

    feature: auth API config

---
+++ src/api/axios.tsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import Auth from "./Auth";
+
+
+const BaseURL = process.env.SHOPERY_API_URL_LOCAL
+
+export const axiosPublic = axios.create({
+    baseURL: BaseURL,
+})
+
+export const axiosPrivate = axios.create({
+    baseURL: BaseURL,
+    headers: {
+        "Content-Type": "application/json"
+    },
+})
+
+const api = {
+    auth: new Auth(axiosPublic, axiosPrivate)
+}
+
+axiosPrivate.interceptors.request.use(
+    function (config: any) {
+        const authToken = JSON.parse(localStorage.getItem("authToken") as any)
+
+        config.headers.authorization = `Bearer: ${authToken}`
+        return config
+    },
+    function (error: any) {
+        return Promise.reject(error)
+    }
+)
+
+axiosPrivate.interceptors.response.use(
+    function (response) {
+        return response
+    },
+    function (error) {
+        if (error?.response?.status === 403) {
+            localStorage.removeItem("authToken")
+            window.location.href = "/loggin"
+        }
+        return Promise.reject(error)
+    }
+)
+
+export default api
