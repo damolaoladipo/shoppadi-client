8abe80d9e38d937c4f2aecfed609e0f66ff4fee0
Damola Oladipo <damolaoladipo@users.noreply.github.com>
2025-01-15 08:06:56 +0100

    feature: authentication API

---
+++ src/api/auth.tsx
@@ -0,0 +1,28 @@
+class Auth {
+    client;
+    secondaryClient;
+  
+    constructor(client: any, secondaryClient?: any) {
+      this.client = client;
+      this.secondaryClient = secondaryClient;
+    }
+
+    register(payload: any) {
+        return this.client.post("/auth/register", payload)
+    }
+  
+    login(payload: any) {
+      return this.client.post("/auth/login", payload);
+    }
+  
+    forgotPassword(payload: any) {
+      return this.client.put("/auth/forgot-password", payload);
+    }
+
+    changePassword(payload: any) {
+        return this.secondaryClient.put("/auth/change-password", payload);
+      }
+  }
+  
+  export default Auth;
+  
